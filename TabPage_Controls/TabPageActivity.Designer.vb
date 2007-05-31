cbe751dbb77c183b3420d6ca3228fc39720ad437
Jarrad Rigano <jarrad.rigano@oceaninformatics.com>
2007-05-31 02:05:13 +0000

    EDT-44 Multiple activities per instruction

--- TabPage_Controls/TabPageActivity.Designer.vb
+++ TabPage_Controls/TabPageActivity.Designer.vb
@@ -30,6 +30,9 @@ Partial Class TabPageActivity
         Me.butGetAction = New System.Windows.Forms.Button
         Me.txtAction = New System.Windows.Forms.TextBox
         Me.HelpProviderActivity = New System.Windows.Forms.HelpProvider
+        Me.openToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.selectToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.RemoveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
         Me.PanelAction.SuspendLayout()
         Me.ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
@@ -44,15 +47,16 @@ Partial Class TabPageActivity
         Me.PanelAction.Controls.Add(Me.txtAction)
         Me.PanelAction.Dock = System.Windows.Forms.DockStyle.Top
         Me.PanelAction.Location = New System.Drawing.Point(0, 0)
+        Me.PanelAction.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.PanelAction.Name = "PanelAction"
-        Me.PanelAction.Size = New System.Drawing.Size(623, 46)
+        Me.PanelAction.Size = New System.Drawing.Size(467, 37)
         Me.PanelAction.TabIndex = 3
         '
         'ContextMenuStrip1
         '
-        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RenameToolStripMenuItem})
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RenameToolStripMenuItem, Me.RemoveToolStripMenuItem})
         Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(153, 48)
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(153, 70)
         '
         'RenameToolStripMenuItem
         '
@@ -63,56 +67,64 @@ Partial Class TabPageActivity
         'butOpenArchetype
         '
         Me.butOpenArchetype.Image = CType(resources.GetObject("butOpenArchetype.Image"), System.Drawing.Image)
-        Me.butOpenArchetype.Location = New System.Drawing.Point(336, 8)
+        Me.butOpenArchetype.Location = New System.Drawing.Point(252, 6)
+        Me.butOpenArchetype.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.butOpenArchetype.Name = "butOpenArchetype"
-        Me.butOpenArchetype.Size = New System.Drawing.Size(24, 24)
+        Me.butOpenArchetype.Size = New System.Drawing.Size(18, 20)
         Me.butOpenArchetype.TabIndex = 5
-        'JAR: 07MAY2007, EDT-28 Display tool tip
-        Dim openToolTip As New ToolTip
-        openToolTip.SetToolTip(butOpenArchetype, "View embedded archetype")
+        Me.openToolTip.SetToolTip(Me.butOpenArchetype, "View embedded archetype")
         '
         'lblNodeId
         '
         Me.lblNodeId.Dock = System.Windows.Forms.DockStyle.Right
         Me.lblNodeId.ForeColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.lblNodeId.Location = New System.Drawing.Point(567, 0)
+        Me.lblNodeId.Location = New System.Drawing.Point(425, 0)
+        Me.lblNodeId.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblNodeId.Name = "lblNodeId"
-        Me.lblNodeId.Size = New System.Drawing.Size(56, 46)
+        Me.lblNodeId.Size = New System.Drawing.Size(42, 37)
         Me.lblNodeId.TabIndex = 4
         '
         'lblAction
         '
-        Me.lblAction.Location = New System.Drawing.Point(8, 11)
+        Me.lblAction.Location = New System.Drawing.Point(6, 9)
+        Me.lblAction.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.lblAction.Name = "lblAction"
-        Me.lblAction.Size = New System.Drawing.Size(64, 16)
+        Me.lblAction.Size = New System.Drawing.Size(48, 13)
         Me.lblAction.TabIndex = 3
         Me.lblAction.Text = "Action"
         '
         'butGetAction
         '
-        Me.butGetAction.Location = New System.Drawing.Point(296, 8)
+        Me.butGetAction.Location = New System.Drawing.Point(222, 6)
+        Me.butGetAction.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.butGetAction.Name = "butGetAction"
-        Me.butGetAction.Size = New System.Drawing.Size(32, 24)
+        Me.butGetAction.Size = New System.Drawing.Size(24, 20)
         Me.butGetAction.TabIndex = 2
         Me.butGetAction.Text = "..."
-        'JAR: 07MAY2007, EDT-28 Display tool tip
-        Dim selectToolTip As New ToolTip
-        selectToolTip.SetToolTip(butGetAction, "Browse for embedded archetype")
+        Me.selectToolTip.SetToolTip(Me.butGetAction, "Browse for embedded archetype")
         '
         'txtAction
         '
-        Me.txtAction.Location = New System.Drawing.Point(72, 8)
+        Me.txtAction.Location = New System.Drawing.Point(54, 6)
+        Me.txtAction.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.txtAction.Name = "txtAction"
-        Me.txtAction.Size = New System.Drawing.Size(216, 22)
+        Me.txtAction.Size = New System.Drawing.Size(163, 20)
         Me.txtAction.TabIndex = 1
+        '
+        'RemoveToolStripMenuItem
+        '
+        Me.RemoveToolStripMenuItem.Name = "RemoveToolStripMenuItem"
+        Me.RemoveToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.RemoveToolStripMenuItem.Text = "Remove"
         '
         'TabPageActivity
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.Controls.Add(Me.PanelAction)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "TabPageActivity"
-        Me.Size = New System.Drawing.Size(623, 376)
+        Me.Size = New System.Drawing.Size(467, 306)
         Me.PanelAction.ResumeLayout(False)
         Me.PanelAction.PerformLayout()
         Me.ContextMenuStrip1.ResumeLayout(False)
@@ -128,5 +140,8 @@ Partial Class TabPageActivity
     Friend WithEvents HelpProviderActivity As System.Windows.Forms.HelpProvider
     Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
     Friend WithEvents RenameToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents openToolTip As System.Windows.Forms.ToolTip
+    Friend WithEvents selectToolTip As System.Windows.Forms.ToolTip
+    Friend WithEvents RemoveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
 
 End Class
