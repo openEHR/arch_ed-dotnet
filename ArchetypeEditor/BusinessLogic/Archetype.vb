3074b9b947753e42e701c3e44fda2f73ada22999
Peter Gummer <peter.gummer@oceaninformatics.com>
2007-10-18 04:41:41 +0000

    EDT-106: Prevent a NullReferenceException when switching from XML to ADL on the Display tab for a Single.

--- ArchetypeEditor/BusinessLogic/Archetype.vb
+++ ArchetypeEditor/BusinessLogic/Archetype.vb
@@ -148,6 +148,7 @@ Public MustInherit Class Archetype
             Case Else
                 Debug.Assert(False, String.Format("Type not handled: {0}", mArchetypeID.ReferenceModelEntity))
         End Select
+
         ReferenceModel.SetArchetypedClass(mArchetypeID.ReferenceModelEntity)
 
         'Not archetype is not synchronised with definition
@@ -155,11 +156,14 @@ Public MustInherit Class Archetype
     End Sub
 
     Public Sub ResetDefinitions()
-        Debug.Assert(Not cDefinition Is Nothing)
-        If Not cDefinition Is Nothing AndAlso TypeOf (cDefinition) Is ArchetypeDefinition Then
-            CType(cDefinition, ArchetypeDefinition).Data.Clear()
+        If Not cDefinition Is Nothing Then
+            If TypeOf (cDefinition) Is ArchetypeDefinition Then
+                CType(cDefinition, ArchetypeDefinition).Data.Clear()
+            End If
+
+            cDefinition.RootLinks.Clear()
         End If
-        cDefinition.RootLinks.Clear()
+
         'Not archetype is not synchronised with definition
         mSynchronised = False
     End Sub
