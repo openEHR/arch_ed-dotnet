a3d50243938239efb10e4ed3dc783998aeea3266
Peter Gummer <peter.gummer@oceaninformatics.com>
2007-11-09 14:23:32 +0000

    EDT-139: On the Definition tab, on the Pathway sub-tab, if you selected the Add Machine State option and then cancelled, instead of cancelling it added the new state anyway. Cancelling now cancels! Also, there was a Properties menu option which did nothing; it is now removed.
    In addition to fixing the above bugs, the Pathway tab's user interface is now slightly enhanced:
    1. Double-clicking a panel allows you to add a state (the same as if you selected Add Machine State from the context menu).
    2. The states now have keyboard focus, so you can use the Tab key to move between them and you can open the context menu via the keyboard.
    3. On adding a new state, its text is now initialised to the name of the state ("Initial", etc.), which often avoid the need to type this in manually.

--- GUI_Classes/Instruction_Controls/PathwayEvent.vb
+++ GUI_Classes/Instruction_Controls/PathwayEvent.vb
@@ -8,6 +8,7 @@ Public Class PathwayEvent
     Private mText As String
     Private mDescription As String
     Private mItem As RmPathwayStep
+    Private mLastEditWasOk As Boolean
     Friend WithEvents toolTipPathway As System.Windows.Forms.ToolTip
     Private mFileManager As FileManagerLocal
 
@@ -30,17 +31,16 @@ Public Class PathwayEvent
         End If
     End Sub
 
-    Sub New(ByVal DefaultMachineStateType As StateMachineType, ByVal a_filemanager As FileManagerLocal)
+    Sub New(ByVal defaultMachineStateType As StateMachineType, ByVal a_filemanager As FileManagerLocal)
 
         'This call is required by the Windows Form Designer.
         InitializeComponent()
 
         mFileManager = a_filemanager
-        Dim a_term As RmTerm = mFileManager.OntologyManager.AddTerm("New Pathway step")
+        Dim a_term As RmTerm = mFileManager.OntologyManager.AddTerm(defaultMachineStateType.ToString)
         mText = a_term.Text
-        mItem = New RmPathwayStep(a_term.Code, DefaultMachineStateType)
-        Me.BackColor = OceanArchetypeEditor.Instance.Options.StateMachineColour(DefaultMachineStateType)
-        MenuEdit_Click(Me, New EventArgs)
+        mItem = New RmPathwayStep(a_term.Code, defaultMachineStateType)
+        BackColor = OceanArchetypeEditor.Instance.Options.StateMachineColour(defaultMachineStateType)
     End Sub
 
     Sub New(ByVal rm As RmPathwayStep, ByVal a_filemanager As FileManagerLocal)
@@ -78,19 +78,17 @@ Public Class PathwayEvent
     Friend WithEvents ContextMenuPathwayEvent As System.Windows.Forms.ContextMenu
     Friend WithEvents MenuEdit As System.Windows.Forms.MenuItem
     Friend WithEvents MenuDelete As System.Windows.Forms.MenuItem
-    Friend WithEvents MenuItem1 As System.Windows.Forms.MenuItem
     <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container
         Me.ContextMenuPathwayEvent = New System.Windows.Forms.ContextMenu
         Me.MenuEdit = New System.Windows.Forms.MenuItem
         Me.MenuDelete = New System.Windows.Forms.MenuItem
-        Me.MenuItem1 = New System.Windows.Forms.MenuItem
         Me.toolTipPathway = New System.Windows.Forms.ToolTip(Me.components)
         Me.SuspendLayout()
         '
         'ContextMenuPathwayEvent
         '
-        Me.ContextMenuPathwayEvent.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.MenuEdit, Me.MenuDelete, Me.MenuItem1})
+        Me.ContextMenuPathwayEvent.MenuItems.AddRange(New System.Windows.Forms.MenuItem() {Me.MenuEdit, Me.MenuDelete})
         '
         'MenuEdit
         '
@@ -101,11 +99,6 @@ Public Class PathwayEvent
         '
         Me.MenuDelete.Index = 1
         Me.MenuDelete.Text = "Delete"
-        '
-        'MenuItem1
-        '
-        Me.MenuItem1.Index = 2
-        Me.MenuItem1.Text = "Properties"
         '
         'toolTipPathway
         '
@@ -131,6 +124,7 @@ Public Class PathwayEvent
             mItem = Value
         End Set
     End Property
+
     Property Selected() As Boolean
         Get
             Return mSelected
@@ -138,18 +132,21 @@ Public Class PathwayEvent
         Set(ByVal Value As Boolean)
             If mSelected <> Value Then
                 mSelected = Value
-                TextRectangle(mText, Me.CreateGraphics)
+                TextRectangle(mText, CreateGraphics)
+
                 If Value Then
                     RaiseEvent SelectionChanged(Me, New EventArgs)
                 End If
             End If
         End Set
     End Property
+
     Shared ReadOnly Property DefaultWidth() As Integer
         Get
             Return 80
         End Get
     End Property
+
     Property AlternativeState() As StateMachineType
         Get
             Return mItem.AlternativeState
@@ -164,6 +161,7 @@ Public Class PathwayEvent
             TextRectangle(mText, Me.CreateGraphics)
         End Set
     End Property
+
     Property DefaultStateMachineType() As StateMachineType
         Get
             Return mItem.StateType
@@ -173,6 +171,7 @@ Public Class PathwayEvent
             Me.BackColor = OceanArchetypeEditor.Instance.Options.StateMachineColour(Value)
         End Set
     End Property
+
     Property PathwayEventText() As String
         Get
             Return mText
@@ -183,6 +182,13 @@ Public Class PathwayEvent
             TextRectangle(mText, Me.CreateGraphics)
         End Set
     End Property
+
+    ReadOnly Property LastEditWasOk() As Boolean
+        Get
+            Return mLastEditWasOk
+        End Get
+    End Property
+
     Public Sub Translate()
         Dim a_term As RmTerm
 
@@ -192,17 +198,38 @@ Public Class PathwayEvent
         Me.toolTipPathway.SetToolTip(Me, mText)
         TextRectangle(mText, Me.CreateGraphics)
     End Sub
+
+    Public Sub Edit()
+        Dim a_term As RmTerm = New RmTerm(mItem.NodeId)
+        a_term.Text = mText
+        a_term.Description = mDescription
+
+        Debug.Assert(mItem.StateType <> StateMachineType.Not_Set)
+
+        Dim s() As String = OceanArchetypeEditor.Instance.GetInput(a_term, ParentForm)
+        mLastEditWasOk = s(0) <> ""
+
+        If mLastEditWasOk Then
+            mText = a_term.Text
+            mDescription = a_term.Description
+            mFileManager.OntologyManager.SetText(a_term)
+            toolTipPathway.SetToolTip(Me, mText)
+            TextRectangle(mText, CreateGraphics)
+            mFileManager.FileEdited = True
+        End If
+    End Sub
+
     Private Function TextRectangle(ByVal TextToRender As String, ByVal g As Graphics) As Drawing.Rectangle
-        Dim fontFamily As New fontFamily("Arial")
+        Dim fontFamily As New FontFamily("Arial")
         Dim a_colour As Drawing.Color
-        Dim font As New font( _
+        Dim font As New Font( _
            fontFamily, _
            8, _
            FontStyle.Regular, _
            GraphicsUnit.Point)
         Dim rect As New Rectangle(4, 4, Me.Width - 8, Me.Height - 8)
-        Dim stringFormat As New stringFormat
-        Dim solidBrush As New solidBrush(Color.FromArgb(255, 0, 0, 0))  'black
+        Dim stringFormat As New StringFormat
+        Dim solidBrush As New SolidBrush(Color.FromArgb(255, 0, 0, 0))  'black
 
         If mItem Is Nothing Then
             a_colour = Me.BackColor
@@ -210,7 +237,7 @@ Public Class PathwayEvent
             'If Not mItem.AlternativeState = StateMachineType.Not_Set Then
             'a_colour = OceanArchetypeEditor.Instance.Options.StateMachineColour(mItem.AlternativeState)
             'Else
-                a_colour = OceanArchetypeEditor.Instance.Options.StateMachineColour(mItem.StateType)
+            a_colour = OceanArchetypeEditor.Instance.Options.StateMachineColour(mItem.StateType)
             'End If
         End If
 
@@ -227,69 +254,45 @@ Public Class PathwayEvent
         stringFormat.LineAlignment = StringAlignment.Center
 
         g.DrawString(TextToRender, font, solidBrush, RectangleF.op_Implicit(rect), stringFormat)
-        Dim pen As New pen(Color.Black)
+        Dim pen As New Pen(Color.Black)
 
         If mSelected Then
             pen.Width = 3
         End If
 
         g.DrawRectangle(pen, rect)
-
-
     End Function
 
     Protected Overrides Sub OnPaint(ByVal e As PaintEventArgs)
         TextRectangle(mText, e.Graphics)
     End Sub
 
+    Private Sub PathwayEvent_GotFocus(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.GotFocus
+        Me.Select()
+        Selected = True
+    End Sub
+
     Private Sub MenuEdit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuEdit.Click, Me.DoubleClick
-        Dim s() As String
-        Dim a_term As RmTerm
-
-        a_term = New RmTerm(Me.mItem.NodeId)
-        a_term.Text = mText
-        a_term.Description = mDescription
-
-        Debug.Assert(mItem.StateType <> StateMachineType.Not_Set)
-
-        s = OceanArchetypeEditor.Instance.GetInput(a_term, Me.ParentForm)
-        If s(0) = "" Then ' cancelled
-            Return
-        End If
-
-        mText = a_term.Text
-        mDescription = a_term.Description
-        mFileManager.OntologyManager.SetText(a_term)
-        Me.toolTipPathway.SetToolTip(Me, mText)
-        TextRectangle(mText, Me.CreateGraphics)
-        mFileManager.FileEdited = True
-
+        Edit()
     End Sub
 
     Private Sub PathwayEvent_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Resize
-        TextRectangle(mText, Me.CreateGraphics)
-    End Sub
-
-    Private Sub PathwayEvent_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles MyBase.Click
-        Me.Select()
-        If Not mSelected Then
-            Me.Selected = True
-        End If
+        TextRectangle(mText, CreateGraphics)
     End Sub
 
     Public Sub Remove()
         Dim p As Panel
         ' get the parent - needed to layout controls as basis for the event
 
-        p = Me.Parent
+        p = Parent
         p.Controls.Remove(Me)
-        Me.mItem = Nothing
+        mItem = Nothing
         mFileManager.FileEdited = True
         RaiseEvent Deleted(p, New EventArgs)
     End Sub
 
     Private Sub MenuDelete_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MenuDelete.Click
-        Me.Remove()
+        Remove()
     End Sub
 
 End Class
