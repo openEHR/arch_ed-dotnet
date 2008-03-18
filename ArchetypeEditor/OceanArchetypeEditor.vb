d0796160242bf06a98f4e091ad349e66c0edeb1f
Peter Gummer <peter.gummer@oceaninformatics.com>
2008-03-18 03:54:02 +0000

    EDT-222: Fix a bug caused by revision 222, which enabled XP visual styles in order to improve the appearance of the web search form. The call to enable visual styles must appear at the very start of the application; otherwise the main window's toolbar icons become invisible.

--- ArchetypeEditor/OceanArchetypeEditor.vb
+++ ArchetypeEditor/OceanArchetypeEditor.vb
@@ -684,6 +684,10 @@ Public Class OceanArchetypeEditor
     End Sub
 
     Shared Sub main(ByVal CmdArgs() As String)
+        ' Enable XP visual style in order to show groups in the web lookup form.
+        ' IMPORTANT! This must be the first call in the application; otherwise the icons do not display on the main toolbar!
+        Application.EnableVisualStyles()
+
         ShowSplash()
 
 #Const TEST_LANGUAGE_TRANSLATION = False
@@ -779,9 +783,6 @@ Public Class OceanArchetypeEditor
         'Catch
         '    Stop
         'End Try
-
-        'To show groups in the web lookup
-        Application.EnableVisualStyles()
 
         Try
             frm.ShowDialog()
