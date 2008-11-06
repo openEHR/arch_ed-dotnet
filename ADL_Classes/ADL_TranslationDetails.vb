f69ec32b2949dee94798111aaa7248a3a4eb05a1
Peter Gummer <peter.gummer@oceaninformatics.com>
2008-11-06 06:06:40 +0000

    EDT-393: On the Description tab, Translation sub-tab, the Accreditation field was not being saved to ADL because a few lines of code were commented out. Uncommenting the code fixes the problem.

--- ADL_Classes/ADL_TranslationDetails.vb
+++ ADL_Classes/ADL_TranslationDetails.vb
@@ -7,33 +7,42 @@ Public Class ADL_TranslationDetails
 
     Public Property ADL_Translation() As openehr.openehr.rm.common.resource.TRANSLATION_DETAILS
         Get
-            SetAuthorValue("name", Me.AuthorName)
-            SetAuthorValue("email", Me.AuthorEmail)
-            SetAuthorValue("organisation", Me.AuthorOrganisation)
-            'If Me.Accreditation <> "" Then
-            '    mADL_Translation.set_accreditation(EiffelKernel.Create.STRING_8.make_from_cil(Me.Accreditation))
-            'End If
+            SetAuthorValue("name", AuthorName)
+            SetAuthorValue("email", AuthorEmail)
+            SetAuthorValue("organisation", AuthorOrganisation)
+
+            If Accreditation <> "" Then
+                mADL_Translation.set_accreditation(EiffelKernel.Create.STRING_8.make_from_cil(Accreditation))
+            End If
+
             Return mADL_Translation
         End Get
         Set(ByVal value As openehr.openehr.rm.common.resource.TRANSLATION_DETAILS)
             mADL_Translation = value
-            Me.Language = value.language.code_string.to_cil
+            Language = value.language.code_string.to_cil
+
             If Not value.accreditation Is Nothing Then
-                Me.Accreditation = value.accreditation.to_cil
+                Accreditation = value.accreditation.to_cil
             End If
+
             'get author details
             Dim s As EiffelKernel.STRING_8
             s = EiffelKernel.Create.STRING_8.make_from_cil("name")
+
             If value.author.has(s) Then
-                Me.AuthorName = CType(value.author.item(s), EiffelKernel.STRING_8).to_cil
+                AuthorName = CType(value.author.item(s), EiffelKernel.STRING_8).to_cil
             End If
+
             s = EiffelKernel.Create.STRING_8.make_from_cil("email")
+
             If value.author.has(s) Then
-                Me.AuthorEmail = CType(value.author.item(s), EiffelKernel.STRING_8).to_cil
+                AuthorEmail = CType(value.author.item(s), EiffelKernel.STRING_8).to_cil
             End If
+
             s = EiffelKernel.Create.STRING_8.make_from_cil("organisation")
+
             If value.author.has(s) Then
-                Me.AuthorOrganisation = CType(value.author.item(s), EiffelKernel.STRING_8).to_cil
+                AuthorOrganisation = CType(value.author.item(s), EiffelKernel.STRING_8).to_cil
             End If
         End Set
     End Property
@@ -48,18 +57,15 @@ Public Class ADL_TranslationDetails
 
             If mADL_Translation.author.has(adlKey) Then
                 mADL_Translation.author.replace(adlValue, adlKey)
-            Else
-                ' only add the value if it is not empty
-                If a_value <> "" Then
-                    mADL_Translation.author.put(adlValue, adlKey)
-                End If
+            ElseIf a_value <> "" Then
+                mADL_Translation.author.put(adlValue, adlKey)
             End If
         End If
     End Sub
 
     Sub New(ByVal a_language As String)
         mADL_Translation = openehr.openehr.rm.common.resource.Create.TRANSLATION_DETAILS.make_from_language(EiffelKernel.Create.STRING_8.make_from_cil(a_language))
-        Me.Language = a_language
+        Language = a_language
     End Sub
 
     Sub New(ByVal a_translation As openehr.openehr.rm.common.resource.TRANSLATION_DETAILS)
@@ -68,11 +74,11 @@ Public Class ADL_TranslationDetails
 
     Sub New(ByVal a_translation As TranslationDetails)
         mADL_Translation = openehr.openehr.rm.common.resource.Create.TRANSLATION_DETAILS.make_from_language(EiffelKernel.Create.STRING_8.make_from_cil(a_translation.Language))
-        Me.Language = a_translation.Language
-        Me.Accreditation = a_translation.Accreditation
-        Me.AuthorName = a_translation.AuthorName
-        Me.AuthorEmail = a_translation.AuthorEmail
-        Me.AuthorOrganisation = a_translation.AuthorOrganisation
+        Language = a_translation.Language
+        Accreditation = a_translation.Accreditation
+        AuthorName = a_translation.AuthorName
+        AuthorEmail = a_translation.AuthorEmail
+        AuthorOrganisation = a_translation.AuthorOrganisation
     End Sub
 
 End Class
