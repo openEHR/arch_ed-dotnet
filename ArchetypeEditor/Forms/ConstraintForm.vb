686ba9ae97c48d2d6e7c7f070dbf7bd7b1ae536e
Peter Gummer <peter.gummer@oceaninformatics.com>
2010-11-04 04:54:05 +0000

    EDT-635: Increase the height of the dialog for editing runtime name constraints. It was too small when internal code or terminology constraints.

--- ArchetypeEditor/Forms/ConstraintForm.vb
+++ ArchetypeEditor/Forms/ConstraintForm.vb
@@ -80,9 +80,9 @@ Public Class ConstraintForm
         Me.PanelBottom.Controls.Add(Me.butCancel)
         Me.PanelBottom.Controls.Add(Me.butOK)
         Me.PanelBottom.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.PanelBottom.Location = New System.Drawing.Point(0, 233)
+        Me.PanelBottom.Location = New System.Drawing.Point(0, 391)
         Me.PanelBottom.Name = "PanelBottom"
-        Me.PanelBottom.Size = New System.Drawing.Size(489, 32)
+        Me.PanelBottom.Size = New System.Drawing.Size(492, 32)
         Me.PanelBottom.TabIndex = 0
         '
         'butDelete
@@ -97,7 +97,7 @@ Public Class ConstraintForm
         '
         Me.butCancel.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.butCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.butCancel.Location = New System.Drawing.Point(392, 4)
+        Me.butCancel.Location = New System.Drawing.Point(395, 4)
         Me.butCancel.Name = "butCancel"
         Me.butCancel.Size = New System.Drawing.Size(88, 24)
         Me.butCancel.TabIndex = 2
@@ -107,7 +107,7 @@ Public Class ConstraintForm
         '
         Me.butOK.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.butOK.DialogResult = System.Windows.Forms.DialogResult.OK
-        Me.butOK.Location = New System.Drawing.Point(292, 4)
+        Me.butOK.Location = New System.Drawing.Point(295, 4)
         Me.butOK.Name = "butOK"
         Me.butOK.Size = New System.Drawing.Size(88, 24)
         Me.butOK.TabIndex = 1
@@ -118,12 +118,13 @@ Public Class ConstraintForm
         Me.AcceptButton = Me.butOK
         Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
         Me.CancelButton = Me.butCancel
-        Me.ClientSize = New System.Drawing.Size(489, 265)
+        Me.ClientSize = New System.Drawing.Size(492, 423)
         Me.ControlBox = False
         Me.Controls.Add(Me.PanelBottom)
         Me.HelpProviderConstraintForm.SetHelpKeyword(Me, "HowTo/Edit data/Set_runtime_name.html")
         Me.HelpProviderConstraintForm.SetHelpNavigator(Me, System.Windows.Forms.HelpNavigator.Topic)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MinimumSize = New System.Drawing.Size(500, 450)
         Me.Name = "ConstraintForm"
         Me.HelpProviderConstraintForm.SetShowHelp(Me, True)
         Me.Text = "ConstraintForm"
