5d1f012474d2ef209d6d285be3f72713a622ce05
Peter Gummer <peter.gummer@oceaninformatics.com>
2012-10-12 02:08:57 +0000

    Declare the TerminologyLookup assembly as CLS-compliant to fix a warning in the main assembly.

--- ArchetypeEditor/TerminologyLookup/Properties/AssemblyInfo.cs
+++ ArchetypeEditor/TerminologyLookup/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
