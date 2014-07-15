openEHR/arch_ed-dotnet

commit 38af8897e267b9207b42b905b800905e7cb3d812

Adds namespacing and revision string to other_details archetype attribute. other_details attributes which are not 'natively handled' by AE are now preserved.

ArchetypeEditor/DataConstraints/GUI/TextConstraintControl.vb
@@ -34,7 +34,7 @@ Public Class TextConstraintControl : Inherits ConstraintControl
 
         mFileManager = a_file_manager
         mAllowedValuesDataView = New DataView(mFileManager.OntologyManager.TermDefinitionTable)
-        ConstraintBindingsGrid.BindTables(mFileManager.OntologyManager)
+    ''    ConstraintBindingsGrid.BindTables(mFileManager.OntologyManager)
 
         If Main.Instance.DefaultLanguageCode <> "en" Then
             radioText.Text = Filemanager.GetOpenEhrTerm(444, radioText.Text)
@@ -46,7 +46,7 @@ Public Class TextConstraintControl : Inherits ConstraintControl
             ToolTip1.SetToolTip(RemoveTermButton, Filemanager.GetOpenEhrTerm(152, "Remove term"))
             TermConstraintLabel.Text = Filemanager.GetOpenEhrTerm(87, TermConstraintLabel.Text)
             TermConstraintDescriptionLabel.Text = Filemanager.GetOpenEhrTerm(113, TermConstraintDescriptionLabel.Text)
-            ConstraintBindingsGrid.TranslateGui()
+      ''      ConstraintBindingsGrid.TranslateGui()
         End If
     End Sub
 
@@ -118,7 +118,7 @@ Public Class TextConstraintControl : Inherits ConstraintControl
         Me.ListViewSelected = New System.Windows.Forms.ListView
         Me.Code = New System.Windows.Forms.ColumnHeader
         Me.Description = New System.Windows.Forms.ColumnHeader
-        Me.ConstraintBindingsGrid = New ConstraintBindingsControl
+        Me.ConstraintBindingsGrid = New Global.ArchetypeEditor.ConstraintBindingsControl
         Me.gbAllowableValues.SuspendLayout()
         Me.SuspendLayout()
         '

commit 5a8b568a7de878812cd48a352a2f9e54a8803172

EDT-789: Fix a bug introduced by 38af8897e267b9207b42b905b800905e7cb3d812 on 22/04/2014 while adding namespace and revision strings to the other_details archetype attribute. For no apparent reason, the initialisation of the terminology grid of the Text constraint control was commented out, causing it to become completely non-functional and to throw exceptions. This change has now been reverted.

ArchetypeEditor/DataConstraints/GUI/TextConstraintControl.vb
@@ -34,7 +34,7 @@ Public Class TextConstraintControl : Inherits ConstraintControl
 
         mFileManager = a_file_manager
         mAllowedValuesDataView = New DataView(mFileManager.OntologyManager.TermDefinitionTable)
-    ''    ConstraintBindingsGrid.BindTables(mFileManager.OntologyManager)
+        ConstraintBindingsGrid.BindTables(mFileManager.OntologyManager)
 
         If Main.Instance.DefaultLanguageCode <> "en" Then
             radioText.Text = Filemanager.GetOpenEhrTerm(444, radioText.Text)
@@ -46,7 +46,7 @@ Public Class TextConstraintControl : Inherits ConstraintControl
             ToolTip1.SetToolTip(RemoveTermButton, Filemanager.GetOpenEhrTerm(152, "Remove term"))
             TermConstraintLabel.Text = Filemanager.GetOpenEhrTerm(87, TermConstraintLabel.Text)
             TermConstraintDescriptionLabel.Text = Filemanager.GetOpenEhrTerm(113, TermConstraintDescriptionLabel.Text)
-      ''      ConstraintBindingsGrid.TranslateGui()
+            ConstraintBindingsGrid.TranslateGui()
         End If
     End Sub
 